1ff5385793dcacd62048fcebe22f273685e52d3e
bny64 <bny64@1.241.22.183>
2019-10-17 22:13:18 +0900

    update

---
+++ src/main/java/com/web/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	private Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	//method 입력하지 않을 시 default값은 GET
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mnv) {
+		logger.debug("---------- MainController index -----------");		
+		mnv.setViewName("index_layout");
+		return mnv;
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/layout/headerInclude.jsp
@@ -0,0 +1,4 @@
+<link href="/style/layout.css" rel="stylesheet" type="text/css" media="all">
+<script src="/js/common/jquery/jquery.min.js"></script>
+<script src="/js/common/jquery/jquery.backtotop.js"></script>
+<script src="/js/common/jquery/jquery.mobilemenu.js"></script>
