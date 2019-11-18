55c9bef8fe69d98a0be33fe4f6678e01377ee526
배남열 <bny64@naver.com>
2019-11-18 18:05:04 +0900

    2019.11.18 spring-security 적용중

---
+++ src/main/java/com/web/common/security/UserAuthProvider.java
@@ -0,0 +1,9 @@
+package com.web.common.security;
+
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+public class UserAuthProvider extends DaoAuthenticationProvider{
+
+	
+
+}
